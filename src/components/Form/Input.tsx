1f2066370ad2d58997748402195aae20ecef0873
Bruna Schneiders <bruna.schneiders@gmail.com>
2021-06-02 21:07:14 -0300

    create signin page

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,31 @@
+import {
+  Input as ChakraInput,
+  FormLabel,
+  FormControl,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {/* html for deve ser uma referência ao id do input */}
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        id={name}
+        name={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        size="lg"
+        _hover={{ bgColor: "gray.900" }}
+        {...rest}
+      />
+    </FormControl>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1>DashGo</h1>;
+import { Flex, Button, Stack } from "@chakra-ui/react";
+
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        {/* pilha de elementos que terão um espaçamento entre si */}
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
